1440db45cd9aeb2213708c9175580ce178ba8a9e
MarioJuradoSoba243 <mariojuso2011@gmail.com>
2025-11-11 11:00:02 +0100

    Polyfill crypto for Vite dev server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,13 @@
+import { webcrypto } from 'node:crypto';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+
+if (!globalThis.crypto) {
+  Object.defineProperty(globalThis, 'crypto', {
+    value: webcrypto,
+    configurable: true
+  });
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
